e5afe2da3b590704af8511c65157e1393c7083f5
mhreza76 <rezabaiust@gmail.com>
2021-02-07 14:27:12 +0600

    const keyword

---
+++ ES2015/const.ts
@@ -0,0 +1,18 @@
+const d = 20
+// d = 100; we can't change const value
+console.log(d)
+
+const obj1 = {
+    name: "Reza",
+    id: "1105060"
+}
+console.log(obj1.name, " ", obj1.id)
+
+//obj1 = {} //cant possible to assign another object to const object
+//we can change the property of a cont object
+
+obj1.name = "Daud"
+obj1.id = "1105076"
+console.log(obj1.name, " ", obj1.id)
+
+
---
+++ ES2015/scripts/const.js
@@ -0,0 +1,15 @@
+"use strict";
+var d = 20;
+// d = 100; we can't change const value
+console.log(d);
+var obj1 = {
+    name: "Reza",
+    id: "1105060"
+};
+console.log(obj1.name, " ", obj1.id);
+//obj1 = {} //cant possible to assign another object to const object
+//we can change the property of a cont object
+obj1.name = "Daud";
+obj1.id = "1105076";
+console.log(obj1.name, " ", obj1.id);
+//# sourceMappingURL=const.js.map
