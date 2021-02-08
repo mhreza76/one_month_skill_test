088293185743df2e83c771f27e81c7ab1d7509b1
mhreza76 <rezabaiust@gmail.com>
2021-02-08 18:44:07 +0600

    boject destructing

---
+++ ES2015/destructing_object.ts
@@ -0,0 +1,12 @@
+let employee2 = {
+    fname: "Morium",
+    lname: "Akter",
+    gender: "Female",
+};
+
+// Useing alise f for fname
+let {fname: f, lname: l, gender: g} = employee2;
+console.log(f);
+console.log(l);
+console.log(g);
+
---
+++ ES2015/scripts/destructing_object.js
@@ -0,0 +1,12 @@
+"use strict";
+var employee2 = {
+    fname: "Morium",
+    lname: "Akter",
+    gender: "Female",
+};
+// Useing alise f for fname
+var f = employee2.fname, l = employee2.lname, g = employee2.gender;
+console.log(f);
+console.log(l);
+console.log(g);
+//# sourceMappingURL=destructing_object.js.map
