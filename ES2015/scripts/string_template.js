6a2a696c764393b4be37d94879942e601675f7df
mhreza76 <rezabaiust@gmail.com>
2021-02-08 18:55:37 +0600

    string template

---
+++ ES2015/scripts/string_template.js
@@ -0,0 +1,5 @@
+"use strict";
+var name1 = "Reza";
+var greet = "Welcome 'single' \"double\"\n                    " + name1 + " to ...\n                This is third line";
+console.log(greet);
+//# sourceMappingURL=string_template.js.map
---
+++ ES2015/string_template.ts
@@ -0,0 +1,5 @@
+let name1 = "Reza";
+let greet = `Welcome 'single' "double"
+                    ${name1} to ...
+                This is third line`;
+console.log(greet);
