8b8452119e3f7036cb12e8b63152edf1edb9d634
mhreza76 <rezabaiust@gmail.com>
2021-02-11 20:09:05 +0600

    forEach loop

---
+++ ES2015/for_each.ts
@@ -0,0 +1,24 @@
+var num = [60,62,73,76,81];
+function arrayFunc(element, index, array){
+    console.log("arr["+index+"]= "+element);
+};
+num.forEach(arrayFunc);
+
+
+let myMap2 = new Map([
+    ["id: ", "1105060"],
+    ["Dept: ", "CSE"]
+]);
+function mapFunction(value, key, callmap){
+    console.log(key, " : ", value);
+    console.log(myMap2 === callmap);
+
+}
+myMap2.forEach(mapFunction);
+
+var newSet = new Set([42,2,63,3,4,55,35,5]);
+function setFunction(value, key, callset){
+    console.log(value);
+    console.log(newSet === callset);
+}
+newSet.forEach(setFunction);
