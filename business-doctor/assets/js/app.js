23c3db135eeee5d421fccd3e7fad94c4873a1067
mhreza76 <rezabaiust@gmail.com>
2021-02-16 19:44:26 +0600

    Web page design with responsive

---
+++ business-doctor/assets/js/app.js
@@ -0,0 +1,17 @@
+const sr = ScrollReveal({
+    duration: 1500,
+    reset: true
+});
+sr.reveal('.ani', {
+
+});
+sr.reveal('p', {
+    delay: 300
+});
+sr.reveal('img', {
+    delay: 400
+});
+
+sr.reveal('sign-up-ani', {
+    delay: 1000
+});
