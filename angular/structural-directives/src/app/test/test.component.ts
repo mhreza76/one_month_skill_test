69216d4744438bc96b85ed1c09d1c5043c786b2b
mhreza76 <rezabaiust@gmail.com>
2021-02-19 23:28:52 +0600

    ngSwitch Directive

--- angular/structural-directives/src/app/test/test.component.ts
+++ angular/structural-directives/src/app/test/test.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TestComponent implements OnInit {
 
   displayName = false;
+  color = 'green';
   constructor() { }
 
   ngOnInit(): void {
